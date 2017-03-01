39a9952a412fd8b191f024668b74402da297d0ea
Wasim <wasim.sayyed@cuelogic.com>
2017-03-01 20:29:36 +0530

    Added components

---
+++ src/app/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <ul className="nav navbar-nav">
+                            <li><a href="#">Home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
---
+++ src/app/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export class Home extends React.Component {
+    render() {
+        return (
+            <p>In a new Component</p>
+        );
+    }
+}
